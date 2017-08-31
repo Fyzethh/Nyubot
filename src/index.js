7d38bd4c58d1aae88fcae7a0256dd1f536dcde3a
david <dliencura@drivetech.pro>
2017-08-31 02:43:46 -0300

    fix queue system of music and add command cola to see current queue of music

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const fecthVideoInfo = require('youtube-info')
+const ytdl = require('ytdl-core')
 const config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'))
 const utils = require('./utils')
+
+const getYoutubeID = require('get-youtube-id')
 const prefix = config.prefix
+let dispatcher = null
+let voiceChannel = null
 let queue = []
+let queueInfo = []
+let countQueue = 0
 let isPlaying = false
 
 client.login(config.token)
@@ -21,29 +28,26 @@ client.on('message', message => {
   if (msg.startsWith(`${prefix}play`)) {
     if (queue.length > 0 || isPlaying) {
       utils.getID(args, id => {
-        utils.addToQueue(id)
+        addToQueue(id)
         fecthVideoInfo(id, function (err, videoInfo) {
           if (err) throw new Error(err)
+          countQueue++
+          queueInfo.push(`${countQueue}-${videoInfo.title}`)
           message.reply(`** ${videoInfo.title} ** agregado a la cola`)
         })
       })
     } else {
       isPlaying = true
       utils.getID(args, id => {
-        queue.push('placeholder')
-        utils.playMusic(id, message)
+        playMusic(id, message)
         fecthVideoInfo(id, function (err, videoInfo) {
           if (err) throw new Error(err)
           message.reply(`Escuchando ** ${videoInfo.title} **  :man_dancing: `)
+          message.reply(queue)
         })
       })
     }
   }
-  // Ping message
-  if (msg === 'ping') {
-    message.reply('pong')
-  }
-
   // If the message is "!mi avatar"
   if (message.content === `${prefix}mi avatar`) {
   // Send the user's avatar URL
@@ -59,4 +63,39 @@ client.on('message', message => {
       message.reply('Debes ingresar un número de mensajes a eliminar')
     }
   }
+
+  if (msg.startsWith(`${prefix}cola`)) {
+    if (queueInfo.length === 0) {
+      message.reply('no hay música en la cola :frowning2:')
+    } else {
+      message.reply(queueInfo)
+    }
+  }
 })
+
+const playMusic = (id, message) => {
+  voiceChannel = message.member.voiceChannel
+  voiceChannel.join().then(connection => {
+    const stream = ytdl(`https://www.youtube.com/watch?v=${id}`, {
+      filter: 'audioonly'
+    })
+    dispatcher = connection.playStream(stream)
+    dispatcher.on('end', function () {
+      queue.shift()
+      if (queue.length === 0) {
+        queue = []
+        isPlaying = false
+      } else {
+        playMusic(queue[0], message)
+      }
+    })
+  })
+}
+
+const addToQueue = strID => {
+  if (utils.isYoutube(strID)) {
+    queue.push(getYoutubeID(strID))
+  } else {
+    queue.push(strID)
+  }
+}
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,8 @@
 const fs = require('fs')
 const getYoutubeID = require('get-youtube-id')
-const ytdl = require('ytdl-core')
 const request = require('request')
 const config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'))
 const YtApiKey = config.yt_api_key
-let dispatcher = null
-let voiceChannel = null
-let isPlaying = false
-let skipReq = 0
-let queue = []
-let skippers = []
 
 const searchVideo = (query, callback) => {
   const q = encodeURIComponent(query)
@@ -17,28 +10,6 @@ const searchVideo = (query, callback) => {
   request(url, function (err, body) {
     const json = JSON.parse(body)
     callback(json.items[0].id.videoId)
-  })
-}
-const playMusic = (id, message) => {
-  voiceChannel = message.member.voiceChannel
-  voiceChannel.join().then(connection => {
-    const stream = ytdl(`https://www.youtube.com/watch?v=${id}`, {
-      filter: 'audioonly'
-    })
-    skipReq = 0
-    skippers = []
-    dispatcher = connection.playStream(stream)
-    dispatcher.on('end', function () {
-      skipReq = 0
-      skippers = []
-      queue.shit()
-      if (queue.length === 0) {
-        queue = []
-        isPlaying = false
-      } else {
-        playMusic(queue[0], message)
-      }
-    })
   })
 }
 
@@ -52,22 +23,12 @@ const getID = (str, callback) => {
   }
 }
 
-const addToQueue = strID => {
-  if (isYoutube(strID)) {
-    queue.push(getYoutubeID(strID))
-  } else {
-    queue.push(strID)
-  }
-}
-
 const isYoutube = str => {
   return str.toLowerCase().indexOf('youtube.com') > -1
 }
 
 module.exports = {
   searchVideo: searchVideo,
-  playMusic: playMusic,
   getID: getID,
-  addToQueue: addToQueue,
   isYoutube: isYoutube
 }
